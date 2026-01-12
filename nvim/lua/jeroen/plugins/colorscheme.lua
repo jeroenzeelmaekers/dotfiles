15b51b5520b20fa2383acabb693aea20aa6ae2d6
Jeroen Zeelmaekers <mail@jeroenzeelmaekers.com>
2026-01-12 15:20:02 +0100

    feat: make tree float

--- nvim/lua/jeroen/plugins/colorscheme.lua
+++ nvim/lua/jeroen/plugins/colorscheme.lua
@@ -4,5 +4,10 @@ return {
   priority = 1000,
   config = function()
     vim.cmd("colorscheme melange")
+
+    -- Make nvim-tree float window use the same background as Neovim
+    local normal_bg = vim.api.nvim_get_hl(0, { name = "Normal" }).bg
+    vim.api.nvim_set_hl(0, "NvimTreeNormalFloat", { bg = normal_bg })
+    vim.api.nvim_set_hl(0, "NvimTreeFloatBorder", { bg = normal_bg })
   end,
 }
