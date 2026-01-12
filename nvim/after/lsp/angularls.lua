f93e29cd2244e1b98e70562a562f253b4ca8f3e6
Jeroen Zeelmaekers <mail@jeroenzeelmaekers.com>
2026-01-12 19:14:07 +0100

    refactor(nvim): only start angularls in angular projects

--- nvim/after/lsp/angularls.lua
+++ nvim/after/lsp/angularls.lua
@@ -1,3 +1,8 @@
 return {
   filetypes = { "typescript", "html", "typescriptreact", "typescript.tsx", "htmlangular" },
+  root_dir = function(fname)
+    local util = require("lspconfig.util")
+    -- Only start angularls if angular.json exists in the project root
+    return util.root_pattern("angular.json")(fname)
+  end,
 }
