9fdc0fc8d7edd290c78584bde1e70ed4846a4737
Jeroen Zeelmaekers <mail@jeroenzeelmaekers.com>
2026-01-18 20:02:24 +0100

    feat(yazi): add yazi + nvim integration

---
+++ nvim/lua/jeroen/plugins/yazi.lua
@@ -0,0 +1,32 @@
+return {
+  "mikavilpas/yazi.nvim",
+  event = "VeryLazy",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+  keys = {
+    {
+      "<leader>e",
+      "<cmd>Yazi<cr>",
+      desc = "Open yazi at the current file",
+    },
+    {
+      "<leader>cw",
+      "<cmd>Yazi cwd<cr>",
+      desc = "Open yazi in nvim's working directory",
+    },
+    {
+      "<leader>-",
+      "<cmd>Yazi toggle<cr>",
+      desc = "Resume the last yazi session",
+    },
+  },
+  opts = {
+    -- if you want to open yazi instead of netrw
+    open_for_directories = true,
+
+    keymaps = {
+      show_help = "<f1>",
+    },
+  },
+}
