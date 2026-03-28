72c65bc7b9e712e89e3d7cb3babc6dfaa4f665d2
Jeroen Zeelmaekers <mail@jeroenzeelmaekers.com>
2026-03-28 19:59:57 +0100

    feat(neovim): add bufferline

---
+++ nvim/lua/jeroen/plugins/bufferline.lua
@@ -0,0 +1,24 @@
+return {
+  "akinsho/bufferline.nvim",
+  version = "*",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  event = "VimEnter",
+  keys = {
+    { "<S-h>", "<cmd>tabprevious<CR>", desc = "Prev tab" },
+    { "<S-l>", "<cmd>tabnext<CR>", desc = "Next tab" },
+    { "<leader>tn", "<cmd>tabnew<CR>", desc = "New tab" },
+    { "<leader>tx", "<cmd>tabclose<CR>", desc = "Close tab" },
+    { "<leader>to", "<cmd>tabonly<CR>", desc = "Close other tabs" },
+  },
+  config = function()
+    local bufferline = require("bufferline")
+    bufferline.setup({
+      options = {
+        mode = "tabs",
+        always_show_bufferline = false,
+        style_preset = bufferline.style_preset.minimal,
+        offsets = {},
+      },
+    })
+  end,
+}
